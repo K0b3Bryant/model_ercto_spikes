71f75cf72d2c8d2ed2018c2b2ae27885f114c60a
Marco <sejereje1011@gmail.com>
2025-09-01 12:02:10 +0200

    Update worldbank_importer.py

--- utils/import/worldbank_importer.py
+++ utils/import/worldbank_importer.py
@@ -1,13 +1,10 @@
 import wbdata
-import pandas as pd
 import datetime
 
 def worldbank_import(indicators, countries='all', start=None, end=None):
     """ Fetch World Bank data. """
-    if start is None:
-        start = datetime.datetime(2000, 1, 1)
-    if end is None:
-        end = datetime.datetime.today()
     
-    df = wbdata.get_dataframe(indicators, country=countries, data_date=(start, end))
-    return df
+    if start is None: start = datetime.datetime(2000, 1, 1)
+    if end is None: end = datetime.datetime.today()
+        
+    return wbdata.get_dataframe(indicators, country=countries, data_date=(start, end))
