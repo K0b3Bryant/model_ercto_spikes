9e8309be8706e11e54e1671f2e2be40498447ce5
Marco <sejereje1011@gmail.com>
2025-09-01 12:01:31 +0200

    Update yahoo_importer.py

--- utils/import/yahoo_importer.py
+++ utils/import/yahoo_importer.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 def yahoo_import(tickers, start=None, end=None, interval='1d'):
     data = yf.download(tickers, start=start, end=end, interval=interval, group_by='ticker')
-    # Return only adjusted close for simplicity
+    
     if isinstance(tickers, list):
         adj_close = pd.concat([data[t]['Adj Close'] for t in tickers], axis=1)
         adj_close.columns = tickers
