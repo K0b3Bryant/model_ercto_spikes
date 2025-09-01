5abc81cffeabdb693ae8226fecd05f4154186ef8
Marco <sejereje1011@gmail.com>
2025-09-01 11:59:28 +0200

    Create openweathermap_importer.py

---
+++ utils/import/openweathermap_importer.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+def weather_import(city, api_key, start=None, end=None):
+    """ Fetch historical weather data from OpenWeatherMap for a given city. """
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    
+    records = []
+    for item in data['list']:
+        dt = datetime.fromtimestamp(item['dt'])
+        temp = item['main']['temp']
+        humidity = item['main']['humidity']
+        wind_speed = item['wind']['speed']
+        records.append({'date': dt, 'temp': temp, 'humidity': humidity, 'wind_speed': wind_speed})
+    
+    df = pd.DataFrame(records)
+    
+    # Filter by start/end if provided
+    if start:
+        df = df[df['date'] >= pd.to_datetime(start)]
+    if end:
+        df = df[df['date'] <= pd.to_datetime(end)]
+    
+    return df
