764da0c1b85db2c1547f82f74b8f00245ad55b33
Marco <sejereje1011@gmail.com>
2025-09-01 11:36:31 +0200

    Update and rename ts_features.py to features.py

---
+++ features/features.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def features(df:pd.DataFrame) --> pd.DataFrame:
+  """ Generates timeseries features """
+
+  
+
+  
+  
+  # ------ tsfresh ------
+  def ts(self, df:pd.DataFrame, underlying:str, max_timeshift:int = 3, min_timeshift:int = 3):
+  
+    # Failsafe
+    if underlying not in df.columns:
+        raise ValueError(f"Column '{underlying}' not found in DataFrame.")
+
+    features = pd.DataFrame({'date': df.index, f'{underlying}': df[underlying], 'id': 1}).reset_index(drop=True)
+    roller = roll_time_series(features, column_id="id", column_sort="date", min_timeshift=min_timeshift, max_timeshift=max_timeshift, rolling_direction=1)
+    roller = roller.fillna(0)
+    logging.basicConfig(level=logging.INFO)
+    extracted_features = extract_features(roller, column_id='id', column_sort='date', column_value=underlying, impute_function=impute, show_warnings=False, n_jobs=1) #n_jobs=1
+    extracted_features.index = extracted_features.index.droplevel(0)
+    extracted_features.index.name = 'date'
+    
+    return extracted_features
+
+  # merge features
+  
+
+  return features
